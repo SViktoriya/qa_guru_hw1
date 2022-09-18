b76c22d5726df2e063d561f3d84fb4f17898bffe
Viktoriya <vika.vesna.95@mail.ru>
2022-09-18 15:07:57 +0300

    Добавлен первый тест

---
+++ src/main/java/org/viktoriia/Main.java
@@ -0,0 +1,7 @@
+package org.viktoriia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/org/viktoriia/Test.java
@@ -0,0 +1,13 @@
+package org.viktoriia;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class Test1 {
+
+    @Test
+    void Test1() {
+        Assertions.assertFalse(2 <= 1);
+    }
